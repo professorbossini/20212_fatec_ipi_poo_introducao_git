7afa82757b4188871ac3b18f68062f3d5684d1a6
Rodrigo Bossini <professorbossini@gmail.com>
2021-08-05 16:20:41 -0300

    Adicionando arquivos faltantes do commit anterior

--- Empregado.java
+++ Empregado.java
@@ -6,6 +6,14 @@ public class Empregado{
     private double comissao;
     private double bonus;
 
+    public void setTipo (int tipo){
+        this.tipo = tipo;
+    }
+
+    public int getTipo(){
+        return this.tipo;
+    }
+
     public double calculaSalario(){
         if (tipo == 1){
             return salario;
